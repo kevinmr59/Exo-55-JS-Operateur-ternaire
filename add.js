d121a050310792bc5bfc0913aea9bd94242edbcc
kevinmr59 <kevinmura59610@gmail.com>
2021-11-14 12:05:32 +0100

    ternaire

---
+++ add.js
@@ -0,0 +1,6 @@
+let a=5;
+let b=12;
+let resultat=document.getElementById('resultat');
+resultat.innerHTML=(a===5&&b<=15)?(a+b).toString():(a-b).toString();
+
+document.body.style.backgroundColor = "#AA0000";
